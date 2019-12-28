3a3cccd8ebb8ea781ceb2f06ee2de21e18f67fb7
sukhjinder-sdei <sukhjinder.singh@smartdatainc.net>
2019-12-28 22:13:16 +0530

    User model created

--- src/profiles_project/profiles_api/models.py
+++ src/profiles_project/profiles_api/models.py
@@ -1,80 +1,81 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
 
-#This import will be used for user permission
+# This import will be used for user permission
 from django.contrib.auth.models import PermissionsMixin
 
-#This is required to crearte a UserManager because we are overiding AbstractBaseUser
+# This is required to crearte a UserManager because we are overiding AbstractBaseUser
 from django.contrib.auth.models import BaseUserManager
+
 
 class UserProfileManager(BaseUserManager):
     '''Helps django to work with our custom user model'''
 
-    #Tell the django how to create a new user from a custom user model. We are telling without password we can create a user
-    def create_user(self,email,name,password:None):
+    # Tell the django how to create a new user from a custom user model. We are telling without password we can create a user
+    def create_user(self, email, name, password: None):
         '''Create a new user profile object.'''
 
-        #If email is not passed as argument
-        if  not email:
+        print('create_user method is invoked')
+        # If email is not passed as argument
+        if not email:
             raise ValueError('User must have an email address')
 
-        #Standarize the email address to lowercase
-        email=self.normalize_email(email)
+        # Standarize the email address to lowercase
+        email = self.normalize_email(email)
 
-        #Now create a user
-        user = self.create_user(email=email,name=name)
+        # Now create a user
+        user = self.model(email=email, name=name)
 
-        #We are using set_password because this method will encrypt the password that will be stored in database
+        # We are using set_password because this method will encrypt the password that will be stored in database
         user.set_password(password)
 
-        #Save the user into db
-        user.save(using = self._db)
+        # Save the user into db
+        user.save(using=self._db)
 
-        #Finally return the user object that is just inserted in db
+        # Finally return the user object that is just inserted in db
         return user
 
-    #This method tell the django how to create a super user. password can not be None for super user
-    def create_superuser(self,emai,name,password):
+    # This method tell the django how to create a super user. password can not be None for super user
+    def create_superuser(self, email, name, password):
         '''Create a new super user'''
 
-        #First create  a standard user then convert it to super user
-        user = self.create_user(emai,name,password)
+        # First create  a standard user then convert it to super user
+        user = self.create_user(email, name, password)
 
-        #Convert normal user to super user
-        user.is_superuser=True
-        user.is_staff=True
+        # Convert normal user to super user
+        user.is_superuser = True
+        user.is_staff = True
 
-        #Save the user into db
-        user.save(using = self._db)
+        # Save the user into db
+        user.save(using=self._db)
 
-        #Finally return the user object that is just inserted in db
+        # Finally return the user object that is just inserted in db
         return user
-
 
 
 # Create your models here.
-class UserProfile(AbstractBaseUser,PermissionsMixin):
+class UserProfile(AbstractBaseUser, PermissionsMixin):
     '''This model will represent user profile in our system '''
-    email = models.EmailField(max_length=255,unique=True)
+    email = models.EmailField(max_length=255, unique=True)
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
 
-    #This field is required because we are overriding AbstractBaseUser
+    # This field is required because we are overriding AbstractBaseUser
     is_staff = models.BooleanField(default=False)
 
-    #Object manager is also required because we are overriding AbstractBaseUser.
-    #It provide us functionality to create a admin user or just a regular user
+    # Object manager is also required because we are overriding AbstractBaseUser.
+    # It provide us functionality to create a admin user or just a regular user
 
-    #We need to tell Django how to use our custom User model
+    # We need to tell Django how to use our custom User model
     objestc = UserProfileManager()
 
-    #We are telling system we will login using email. By default email is required field
-    USERNAME_FIELD ='email'
+    # We are telling system we will login using email. By default email is required field
+    USERNAME_FIELD = 'email'
 
-    #All the field that are required. We can add more fields to this list.
+    # All the field that are required. We can add more fields to this list.
     REQUIRED_FIELDS = ['name']
 
-    #Add some helper functions
+    # Add some helper functions
     def get_full_name(self):
         ''' Use to get users full name         '''
 
@@ -90,5 +91,3 @@ class UserProfile(AbstractBaseUser,PermissionsMixin):
         '''This method will convert the object to String '''
 
         return self.email
-
-
